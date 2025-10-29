939b6188c6dc294c1125511a6335e662f0ddce4d
Prajwalais <prajwalbakaningi7@gmail.com>
2025-10-30 01:15:17 +0530

    Student

---
+++ studentt.py
@@ -0,0 +1,18 @@
+# Student Grade Evaluation Program
+
+# Accept marks of a student
+marks = int(input("Enter marks: "))
+
+# Determine grade
+if marks >= 90:
+    grade = "A"
+elif marks >= 75:
+    grade = "B"
+elif marks >= 60:
+    grade = "C"
+elif marks >= 40:
+    grade = "D"
+else:
+    grade = "Fail"
+
+print("Grade:", grade)
